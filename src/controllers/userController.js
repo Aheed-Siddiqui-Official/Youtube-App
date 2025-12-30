f92ce55431350a0b6b2ed102b3b7016ac483aa38
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-12-31 00:02:56 +0500

    How to use postman for backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,7 +16,8 @@ const registerUser = asyncHandler(async (req, res) => {
   //   return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+  //   console.log("email", email);
+  //   console.log("req.body",  req.body);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -24,7 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
 
@@ -33,7 +34,18 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // console.log("req.files", req.files);
+
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // bug resolved
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -67,9 +79,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   return res
     .status(201)
-    .json(
-        new ApiResponse(200, createdUser, "User registered Successfully")
-    );
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
