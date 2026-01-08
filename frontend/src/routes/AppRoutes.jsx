d423f5311c0859c438cca1dd614171b9830609f7
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2026-01-09 00:23:47 +0500

    Creating structure

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from '../pages/Home'
+import Liked from '../pages/Liked'
+import History from '../pages/History'
+import Playlist from '../pages/Playlist'
+import Dashboard from '../pages/Dashboard'
+import Collection from '../pages/Collection'
+import Community from '../pages/Community'
+
+const AppRoutes = () => {
+  return (
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/liked' element={<Liked/>}/>
+        <Route path='/history' element={<History/>}/>
+        <Route path='/playlist' element={<Playlist/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/community' element={<Community/>}/>
+      </Routes>
+  )
+}
+
+export default AppRoutes
